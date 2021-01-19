74c5b930aacb58ee2bd9254eba6420e89a84e17f
camillawilliams <camillawilliamsdu@gmail.com>
2021-01-19 00:22:37 +0000

    added state

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
+export const Home = () => {
+    const [characters, setCharacters] = useState([{name:"Luke", name: "Anakyn"}]);
+    const [planets, setPlanets] = useState([{name:"Death Star", name: "The Republic"}]);
+    return (
+	<div className="container-fluid mt-5">
+        <div>
+		<h1>Characters</h1>
+        <div>
+            {characters.map((value, index) => {
+                return 
+            })}
+        </div>
+        </div>
+		<div>
+			<h1>Planets</h1>
+		</div>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
+        
 	</div>
 );
+ }
