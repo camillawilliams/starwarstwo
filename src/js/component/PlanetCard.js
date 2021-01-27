eaac1cdf77c0b76bef9e09e6a99ae2634522c22f
camillawilliams <camillawilliamsdu@gmail.com>
2021-01-27 01:23:19 +0000

    planets can now add to favorites

--- src/js/component/PlanetCard.js
+++ src/js/component/PlanetCard.js
@@ -1,17 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
-import starwarsPlanet from "../../img/starwarsPlanet.jpg";
+import { Link } from "react-router-dom";
+import starwarsRobot from "../../img/starwarsRobot.jpg";
+import { Context } from "../store/appContext";
 
 export const PlanetCard = props => {
+	const { store, actions } = useContext(Context);
 	return (
 		<div className="card" style={{ width: "18rem" }}>
-			<img className="card-img-top" src={starwarsPlanet} alt="Card image cap" />
+			<img className="card-img-top" src={starwarsRobot} alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.planet.name}</h5>
-				<p className="card-text">{props.planet.climate}</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
+				<p className="card-text">{props.planet.height}</p>
+				<p className="card-text">{props.planet.hair_color}</p>
+				{/* <a href="#" className="btn btn-primary" /> */}
+				<Link to={`/planetdetails/${props.index}`}>
+					{" "}
+					{/* className="btn btn-sm"> */}
+					<span href="#" className="btn btn-primary">
+						CLick for more
+					</span>
+				</Link>
+				<button onClick={() => actions.setFavorite(props.planet.name)}>Add Favorite</button>
+				{/* this button runs an onCLick event. Passes and argument which is the name of the character */}
 			</div>
 		</div>
 	);
@@ -19,7 +30,7 @@ export const PlanetCard = props => {
 
 PlanetCard.propTypes = {
 	planet: PropTypes.object
-
-	// imageURL: PropTypes.string
-	// description: PropTypes.string
+};
+PlanetCard.propTypes = {
+	index: PropTypes.number
 };
